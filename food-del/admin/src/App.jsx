72bd3627311e95eabdf4aec56c09a6001981dccd
kylx <lexuanky391@gmail.com>
2026-02-02 00:56:03 +0700

    add admin page

--- food-del/admin/src/App.jsx
+++ food-del/admin/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from './components/pages/Add/Add';
+import List from './components/pages/List/List';
+import Orders from "./components/pages/Orders/Orders";
 
 const App=()=>{
     return (
@@ -7,7 +12,12 @@ const App=()=>{
             <Navbar/>
             <hr/>
             <div className="app-content">
-                <Silebar/>
+                <Sidebar/>
+                <Routes>
+                    <Route path="/add" element={<Add/>}/>
+                    <Route path="/list" element={<List/>}/>
+                    <Route path="/orders" element={<Orders/>}/>
+                </Routes>
             </div>
         </div>
     )
